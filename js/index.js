f291c45321b889439be58ac8ab9ff75cf24d2cef
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-12 16:16:11 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -1,30 +1,46 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
-const messageInput = document.querySelector('[data-js="form"]');
-const charactersLeftElement = document.querySelector('[data-js="form"]');
-
-messageInput.addEventListener("input", (event) => {
-    //charactersLeftElement.textContent = 150 - event.target.value.length;
-});
 
 form.addEventListener("submit", (event) => {
-    event.defaultPrevented();
+    event.preventDefault(); //unterbindet das Standart Verhalten von einem formular
+  
+    const formData = new FormData(event.target); // gibt alle Daten von einem Formular an
+    const data = Object.fromEntries(formData);
+  
+    console.log(data);
 
-const formElement = event.target.element;
-
-const firstNameValue = formElements.firstName.value;
-const lastNameValue = formElement.lastName.value;
-
-console.log(firstNameValue);
-console.log(lastNameValue);
+    event.target.reset();
+    event.target.elements.firstName.focus(); // der Courser wechselt beim Abschicken vom Formular in den First Name
+  });
 
 
-});
+//const form = document.querySelector('[data-js="form"]');
+//const messageInput = document.querySelector('[data-js="form"]');
+//const charactersLeftElement = document.querySelector('[data-js="form"]');
 
-const formData = new FormData(event.target);
-const data = Object.fromEntries(formData);
+//messageInput.addEventListener("input", (event) => {
+    //charactersLeftElement.textContent = 150 - event.target.value.length;
+//});
 
-console.log(data);
+//form.addEventListener("submit", (event) => {
+ //   event.defaultPrevented();
 
-event.target.removeEventListener();
+//const formElements = event.target.element;
+
+//const firstNameValue = formElements.firstName.value;
+//const lastNameValue = formElements.lastName.value;
+
+//console.log(firstNameValue);
+//console.log(lastNameValue);
+
+//const formData = new FormData(event.target);
+//const data = Object.fromEntries(formData);
+
+//console.log(data);
+
+//event.target.removeEventListener();
+//});
+
+
+
