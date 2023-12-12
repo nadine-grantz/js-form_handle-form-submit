144e3d7b93b7eafc1341d5702d52ce2c0fe8b91c
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-12 15:27:09 +0100

    add challenge

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const messageInput = document.querySelector('[data-js="form"]');
+const charactersLeftElement = document.querySelector('[data-js=""]');
+
+messageInput.addEventListener("input", (event) => {
+    charactersLeftElement.textContent = 150 - event.target.value.length;
+});
+
+form.addEventListener("submit", (event) => {
+    event.defaultPrevented();
+});
+
+const formData = new FormData(event.target);
+const data = Object.fromEntries(formData);
